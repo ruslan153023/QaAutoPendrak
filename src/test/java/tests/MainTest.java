d4ff9e31e2abd41eb3b2b9c7d3a3ca555e087f66
ruslan153023 <ruslan153023@yandex.ru>
2024-09-03 23:20:23 +0300

    add description

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -16,7 +16,7 @@ public class MainTest extends BaseTest {
         Assert.assertTrue(
                 mainPage.setValueAtInputAndEnterShort("Чем iPhone 13 отличается от iPhone 12")
                         .chekContainsOfHref()
-                        .contains("iphone-13"));
+                        .contains("iphone-15"));
     }
 
 
